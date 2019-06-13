0eee13899a99df93c22069ba1a41fca99b16a753
myungwoo-Y <auddn6676@naver.com>
2019-06-13 22:58:42 +0900

    add background image

--- CityList.js
+++ CityList.js
@@ -1,78 +1,78 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import { Constants } from 'expo';
+import {FlatList, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {Constants} from 'expo';
 
 export default class CityList extends React.Component {
-  static navigationOptions = {
-    title: 'Cities',
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      cities: [],
+    static navigationOptions = {
+        title: 'Cities',
     };
-  }
 
-  componentDidMount() {
-    //fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
-      // 각자의 링크
-    fetch('http://989e328a.ngrok.io/weather-crawler/available-cities')
-        .then(response => response.json())
-      .then(cities => {
-        console.log('cities =', cities.length);
-        this.setState({
-          cities
-        });
-      });
-  }
+    constructor(props) {
+        super(props);
 
-  onPressCity(item) {
-    this.props.navigation.navigate(
-      'Detail',
-      {
-        city: item
-      }
-    );
-  }
+        this.state = {
+            cities: [],
+        };
+    }
 
-  renderItem(city) {
-    return (
-      <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
-        <Text style={styles.text}>{city}</Text>
-      </TouchableOpacity>
-    );
-  }
+    componentDidMount() {
+        fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
+        // 각자의 링크
+        //fetch('http://localhost:8100/weather-crawler/available-cities')
+            .then(response => response.json())
+            .then(cities => {
+                console.log('cities =', cities.length);
+                this.setState({
+                    cities
+                });
+            });
+    }
 
-  render() {
-    return (
-      <FlatList style={styles.container}
-                renderItem={({ item }) => this.renderItem(item)}
-                keyExtractor={item => item}
-                data={this.state.cities}
-      />
-    );
-  }
+    onPressCity(item) {
+        this.props.navigation.navigate(
+            'Detail',
+            {
+                city: item
+            }
+        );
+    }
+
+    renderItem(city) {
+        return (
+            <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
+                <Text style={styles.text}>{city}</Text>
+            </TouchableOpacity>
+        );
+    }
+
+    render() {
+        return (
+                <FlatList style={styles.container}
+                          renderItem={({item}) => this.renderItem(item)}
+                          keyExtractor={item => item}
+                          data={this.state.cities}
+                />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: Constants.statusBarHeight,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: Constants.statusBarHeight,
+    },
 
-  item: {
-    flex: 1,
-    height: 50,
-    justifyContent: 'center',
+    item: {
+        flex: 1,
+        height: 50,
+        justifyContent: 'center',
 
-    borderWidth: 1,
-    borderColor: 'orange',
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-  }
+        borderWidth: 1,
+        borderColor: '#808080',
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'left',
+    }
 });
--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -1,59 +1,104 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Constants } from 'expo';
+import {StyleSheet, Text, View} from 'react-native';
+import {Constants} from 'expo';
+import {ImageBackground} from 'react-native';
 
 export default class WeatherDetailScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: `Weather Info: ${navigation.getParam('city', 'Unknown')}`,
+    static navigationOptions = ({navigation}) => {
+        return {
+            title: `Weather Info: ${navigation.getParam('city', 'Unknown')}`,
+        };
     };
-  };
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isLoading: true,
-    };
-  }
-  componentDidMount() {
-    const { navigation } = this.props;
-    const city = navigation.getParam('city', 'Unknown');
-    // 각자의 링크
-    //fetch(`http://localhost:8080/weather-crawler/current-weathers/by-city-name/${city}`)
-      fetch(`http://989e328a.ngrok.io/weather-crawler/current-weathers/by-city-name/${city}`)
-      .then(response => response.json())
-      .then(info => {
-        this.setState({
-          ...info,
-          isLoading: false,
-        });
-      });
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={styles.container}>
-          <Text>데이터를 불러오는 중입니다.</Text>
-        </View>
-      )
+        this.state = {
+            isLoading: true,
+        };
     }
 
-    let celsius = this.state.main.temp - 273.15;
-    return (
-      <View style={styles.container}>
-          <Text>시간 : {new Date().toDateString()} </Text>
-          <Text>온도: {celsius.toFixed(1)}</Text>
-      </View>
-    );
-  }
+    componentDidMount() {
+        const {navigation} = this.props;
+        const city = navigation.getParam('city', 'Unknown');
+        // 각자의 링크
+        //fetch(`http://localhost:8080/weather-crawler/current-weathers/by-city-name/${city}`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=621e61b1abbd67966347e3beb3f2e093`)
+            .then(response => response.json())
+            .then(info => {
+                this.setState({
+                    ...info,
+                    isLoading: false,
+                });
+            });
+    }
+
+    render() {
+
+        let backgrounName = "";
+        let d = new Date();
+        var hour = d.getHours();
+        var imageNumber = 0;
+        var backgroundImages = [
+            require('./images/sunny.jpg'),
+            require('./images/night.jpg'),
+        ];
+
+        if(hour > 6 && hour < 19){
+            imageNumber = 0;
+        }else{
+            imageNumber = 1;
+        }
+
+
+        if (this.state.isLoading) {
+            return (
+                <ImageBackground source={backgroundImages[imageNumber]}
+                                 style={styles.backgroundImage}>
+                    <View style={styles.container}>
+                        <Text style={styles.textWeather}>데이터를 불러오는 중입니다.</Text>
+                    </View>
+                </ImageBackground>
+            )
+        }
+
+
+
+        let celsius = this.state.main.temp - 273.15;
+        return (
+            <ImageBackground source={backgroundImages[imageNumber]}
+                             style={styles.backgroundImage}>
+                <View style={styles.container}>
+                    <Text style={styles.text}>{celsius.toFixed(1)} &deg;C</Text>
+                    <Text style={styles.textWeather}>{new Date().toDateString()} </Text>
+                </View>
+            </ImageBackground>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: Constants.statusBarHeight,
-  },
+    container: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+
+    },
+    text: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: "white",
+        textAlign: 'center',
+    },
+    textWeather: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: "white",
+        textAlign: 'center',
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover', // or 'stretch'
+        width: '100%',
+        height: '100%'
+    },
 });
